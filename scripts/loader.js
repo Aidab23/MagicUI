5952439299a8310456b653871e116d11c3cb6d5c
Aidab23 <72881705+Aidab23@users.noreply.github.com>
2022-11-21 20:40:30 +0000

    Main Setup

--- scripts/loader.js
+++ scripts/loader.js
@@ -5,4 +5,5 @@
         frameCont.innerHTML = "https://raw.githubusercontent.com/Aidab23/MagicGUI/main/resources/gui.html";
         document.querySelector("body").append(frameCont);
     };
+    init();
 })
