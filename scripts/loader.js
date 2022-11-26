7dabe57d7e9162c5b3c045e0a84e9436c94ce422
Aidab23 <72881705+Aidab23@users.noreply.github.com>
2022-11-26 14:12:31 -0600

    Deleted Useless Files

--- scripts/loader.js
+++
@@ -1,9 +0,0 @@
-(function(e){
-    "use strict";
-    function init(e) {
-        var frameCont = document.createElement("div");
-        frameCont.innerHTML = "https://raw.githubusercontent.com/Aidab23/MagicGUI/main/resources/gui.html";
-        document.querySelector("body").append(frameCont);
-    };
-    init();
-})
